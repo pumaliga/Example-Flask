09decb04320a2c8278f2d3da14f80394de8fc26f
Mac Mini <>
2022-02-02 13:21:18 +0200

    config for run app with gunicorn

--- app.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from app import create_app
-from app.models import User
-
-
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
---
+++ gunicorn_conf.py
@@ -0,0 +1,20 @@
+bind = '192.168.1.69:5000'
+backlog = 2048
+workers = 1
+worker_class = 'eventlet'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+user = "macmini"
+spew = False
+errorlog = '-'
+loglevel = 'debug'
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+proc_name = 'Example-Flask'
+
+venv = '/Users/macmini/PycharmProjects/Example/venv'
+
+#  gunicorn "app:create_app()" -c gunicorn_conf.py
+
+# pkill -f gunicorn
